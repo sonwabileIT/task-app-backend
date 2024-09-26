90f4e7d17fab4923dc09abc3c343e3a315736cc7
Sonwabile <sonwabilegxoyiyait@gmail.com>
2024-09-26 16:09:10 +0200

    feat: add express server

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello from the GET server');
 })
+
+//get all tasks
+app.get('/api/tasks', (req, res) => {
+    res.send('Hello from api/tasks');
+})
+
+//mark a task complete
+app.patch('/api/tasks/[0-9]', (req, res) => {
+    res.send('Hello from patch')
+})
+
+//delete a complete
+app.delete('/api/tasks/[0-9]', (req, res) => {
+    res.send('Hello from delete task')
+})
     
 
 
