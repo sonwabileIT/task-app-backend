1f71f57baac3a658d12710fb5f470235ee7536ec
Sonwabile <sonwabilegxoyiyait@gmail.com>
2025-02-11 15:32:00 +0200

    fix: routing issue on aiven

--- routes/tasksRoute.js
+++ routes/tasksRoute.js
@@ -7,21 +7,21 @@ const router = express.Router();
 router.get('/', getHelloMessageController);
 
 //get all tasks
-router.get('/api/tasks', getAllTasksController);
+router.get('api/tasks', getAllTasksController);
 
 //post a task
-router.post('/api/tasks', postTaskController);
+router.post('api/tasks', postTaskController);
 
 //delete all tasks
-router.delete('/api/tasks', deleteAllTasksController);
+router.delete('api/tasks', deleteAllTasksController);
 
 //get a task
-router.get('/api/tasks/:id', getTaskByIdController);
+router.get('api/tasks/:id', getTaskByIdController);
 
 //mark a task complete
-router.patch('/api/tasks/:id', updateTaskCompleteByIDController);
+router.patch('api/tasks/:id', updateTaskCompleteByIDController);
 
 //delete a complete
-router.delete('/api/tasks/:id', deleteTaskByIDController);
+router.delete('api/tasks/:id', deleteTaskByIDController);
 
 export default router;
